13f3855cb0444cbe6e2372fe796ad454d2e53c74
mgesteiro <mgesteiro@mgesteiro.com>
2021-08-08 17:29:11 +0200

    New extension to program an Escornabot Brivoi DIY version

---
+++ RT_EscornabotDIY/src/src/src.ino
@@ -0,0 +1,31 @@
+#include "EscornabotDIY.h"
+
+EscornabotDIY escornabot;
+
+void setup() {
+    Serial.begin(115200);
+    escornabot.playNote(NOTE_BEEP, 100);
+}
+
+void loop() {
+    if (escornabot.isButtonPressed("FORWARD")) {
+        escornabot.playNote(NOTE_BEEP, 100);
+        delay(200);
+        escornabot.move(10);
+    }
+    if (escornabot.isButtonPressed("BACKWARD")) {
+        escornabot.playNote(NOTE_BEEP, 100);
+        delay(200);
+        escornabot.move(-10);
+    }
+    if (escornabot.isButtonPressed("TURN RIGHT")) {
+        escornabot.playNote(NOTE_BEEP, 100);
+        delay(200);
+        escornabot.turn(90);
+    }
+    if (escornabot.isButtonPressed("TURN LEFT")) {
+        escornabot.playNote(NOTE_BEEP, 100);
+        delay(200);
+        escornabot.turn(-90);
+    }
+}
